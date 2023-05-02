6c356832d7a03b4c298ca86976f4cab1b3564a58
TatyanaPro <128369173+TatyanaPro@users.noreply.github.com>
2023-05-02 17:19:16 +0300

    Add files via upload

---
+++ НМ-5/index.js
@@ -0,0 +1,95 @@
+// /*let a = prompt("Введите первое число");
+// let b = prompt("Введите второе число");
+
+// function com (a,b) {
+//     return a<b ? a: b; }
+// console.log(com (a,b));*/
+
+
+// /*let w = prompt("Введите число");
+
+// function checkNumbers(w) {
+//     if (w % 2 == 0) {
+//       return confirm('Число четное');
+//     } else {
+//       return confirm('Число нечетное');
+//     }
+//   }
+// console.log(checkNumbers(w));*/
+
+
+// /*let a = prompt("Введите число");
+
+// function squareNumber (w) {
+//   let number = (w ** 2);
+//   return number
+// } 
+// console.log(squareNumber (a));
+
+
+// /*let z = prompt("Введите число");
+
+// function checkNumbers (z) {
+//     return z ** 2
+// }
+// console.log(checkNumbers (z));*/
+
+
+// /*let ask = prompt("Сколько вам лет?");
+
+// if (ask > 0 && ask <12) {
+//     alert ("Привет, друг!");
+    
+//   } 
+//   else if (ask >=13) {
+//     alert ("Добро пожаловать!");
+    
+//   }
+//   else {
+//     alert ("Вы ввели неправильное значение");
+
+// }*/
+
+// // let r = prompt("Введите число");
+// // let b = prompt("Введите второе число");
+// // 
+// // function multNumbers(num1, num2) {
+//   // if (isNaN(Number(num1)) || isNaN(Number(num2))) {
+//     // return 'Одно или оба значения не являются числом';
+//   // }
+//   // return num1 * num2;
+// // }
+// // console.log(mulNumbers(r,b));
+// // 
+
+// let cube = prompt("Введите число");
+// function checkNumber (cube) {
+  // if (isNaN(cube)) {
+    // return 'Вы ввели неправильное значение'
+  // } else {
+    // return cube * cube * cube;
+//  }
+// }
+// alert (`Вашe число в кубе равняется ${checkNumber(cube)}`);
+// 
+
+
+// const circleOne = {
+  // radius: 13,
+  // getArea() {
+    // return Math.PI * this.radius ** 2;
+  // },
+  // getPerimeter() {
+    // return 2 * Math.PI * this.radius;
+  // }
+// }
+// 
+// const circleTwo = {
+  // radius: 7,
+  // getArea() {
+    // return Math.PI * this.radius ** 2;
+  // },
+  // getPerimeter() {
+    // return 2 * Math.PI * this.radius;
+  // }
+// };
