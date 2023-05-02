8ab162ff2633ca5107f205965a5b3cad193cbf33
TatyanaPro <128369173+TatyanaPro@users.noreply.github.com>
2023-05-02 17:20:48 +0300

    Add files via upload

---
+++ HM -6/index.js
@@ -0,0 +1,92 @@
+// const num = [1, 5, 4, 10, 0, 3]
+// for (let a = 0; a < num.length; a++) {
+	// console.log(num[a]);
+    // if (num[a] === 10) break;
+// }
+
+
+// const vew = [1, 5, 4, 10, 0, 3]
+// console.log(vew.indexOf(4)); 
+
+
+// let numbs = [1, 3, 5, 10, 20]
+// numbs = numbs.join (' ')
+// console.log (numbs);
+
+
+
+let mass = new Array (3);
+for (i = 1; i < 2; i++) {
+    mass[i] = new Array;
+}
+console.log(mass);
+
+
+// let me =[ 1, 1, 1]
+// me.push (2, 2, 2);
+// console.log(me);
+
+
+// let quick = [9, 8, 7, 'a', 6, 5]
+// quick=quick.sort();
+// console.log(quick);
+// quick.pop();
+// console.log(quick);
+
+
+
+// let a = parseInt(prompt ('угадай, какое число есть в списке от 1-10?', ''));
+// let numbs = [9, 8, 7, 6, 5];
+// if (numbs.includes(a)) {
+    // alert(`чило ${a} есть`);
+// } else {
+    // alert(`чилa ${a} нет`)
+// }
+
+
+
+
+// const a = 'abcdef';
+// console.log(a.split('').reverse().join(''));
+// console.log(a);
+
+
+// let numbs =[
+    // [1, 2, 3,],
+    // [4, 5, 6],
+// ];
+// for (let numbsIn of numbs){
+    // for (let element of numbsIn){
+        // console.log(element);
+    // }
+// }
+
+
+// let mony =[4,6,1,8,3]
+// for (let a = 0; a < mony.length; a++) {
+    // console.log(mony[a]+mony[a+1]);
+// }
+
+
+// function cubNumbers (num){
+    // return num.map (aga => (aga*aga))
+// }
+// console.log(cubNumbers([5,2,3]));
+// 
+
+
+
+// function getLengthWords(arr) {
+    // return arr.map ((text) => text.length);
+// }
+// console.log(getLengthWords(['слово', '', 'слог', 'длинное предложение', 'буква']));
+// 
+
+// function otricNumders(num) {
+    // return num.filter (hah => (hah<0));
+// }
+// console.log(otricNumders ([0,-4,7,-5,10,-8]));
+// 
+
+
+
